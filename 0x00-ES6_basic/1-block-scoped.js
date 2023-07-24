87f990f38c636028e4d032d41758bbe805af5b3c
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-24 17:32:36 +0200

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
