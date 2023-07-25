4d108ed6d355fcc064e12d550cb341a4746a55f6
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-25 11:01:33 +0200

    quick fix

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
-export default function concatArrays (array1, array2, string) {
+export default function concatArrays(array1, array2, string) {
   const res = [...array1, ...array2, ...string];
   return res;
 }
