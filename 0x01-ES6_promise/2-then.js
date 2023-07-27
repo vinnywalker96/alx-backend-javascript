61f72360c96a9e7f504e32ea99034412ebdc6319
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-27 16:43:41 +0200

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
-  }).finally(() => {
-    console.log('Got a response from the API');
-  });
-
+    promise.then((response) => {
+        console.log('Got a response from the API');
+    }).catch((error) => {
+        console.log(error);
+    });
 }
