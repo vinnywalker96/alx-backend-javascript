890845476ebbb4d99834cf7deac8782ccdb33101
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-27 17:47:24 +0200

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const success = {
-    status: 200,
-    body: 'success',
-  };
-  const successHandler = () => {
-    console.log('Got a response from the API');
-    return Promise.resolve(success);
-  };
-  const errorHandler = () => Promise.reject(new Error());
-  promise.then(successHandler).catch(errorHandler);
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
