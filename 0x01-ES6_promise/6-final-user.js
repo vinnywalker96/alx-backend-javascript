5ab4d3ee8722984d0fc7c8e067aefe91e7ffc513
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 12:23:53 +0200

    loadbalancer

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from "./5-photo-reject";
 export default  async function handleProfileSignup(firstName, lastName, filename){
     return Promise.all([ await signUpUser(firstName, lastName), 
          await uploadPhoto(filename)]).then((values) => {
-            return [{
+            return [{ 
         
 
             }]
