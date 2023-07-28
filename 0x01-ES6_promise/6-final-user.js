ce394ea20ffa3eb2789644e6b44f2792bde1285e
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 14:23:04 +0200

    quick fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName),
-    uploadPhoto(filename)]).then((values) => Promise.resolve([
-    { status: values.status, value: values.value },
-  ]));
+  try {
+    const user = await signUpUser(firstName, lastName);
+    const photo = await uploadPhoto(filename);
+    return Promise.resolve({
+      status: 'fulfilled',
+      value: {
+        ...user,
+        ...photo,
+      },
+    });
+  } catch (error) {
+    return Promise.resolve({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
 }
