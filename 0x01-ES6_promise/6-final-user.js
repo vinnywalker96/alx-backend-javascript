068d26b680e4d91a542df95be7884cc130ebd351
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 11:43:08 +0200

    Reject the promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default  async function handleProfileSignup(firstName, lastName, filename){
+    return Promise.all([ await signUpUser(firstName, lastName), 
+         await uploadPhoto(filename)]).then((values) => {
+            return [{
+        
+
+            }]
+
+    })
+
+}
