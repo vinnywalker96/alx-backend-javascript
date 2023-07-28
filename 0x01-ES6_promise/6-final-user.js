a070548cb0213052fe681f33588e19074411fdcb
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 12:46:25 +0200

    Multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName),
     uploadPhoto(filename)]).then((values) => Promise.resolve([
-    { status: values[0].status, value: values[0].value },
-    { status: values[1].status, value: values[1].value }
-    ]));
+    { status: values.status, value: values.value },
+  ]));
 }
