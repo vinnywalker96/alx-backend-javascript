d9193a549bb78e0d891c3ff90cad30df7b7980b6
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 13:27:42 +0200

    Await/ Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUse() {
-  return Promise.all([await uploadPhoto(),
+  return Promise.allSettled([await uploadPhoto(),
     await createUser()]).then((values) => Promise.resolve({
     photo: values[0],
     user: values[1],
