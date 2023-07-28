fee00e5a1818ad5e2808c845ab3ab833f9b2d942
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-28 12:41:05 +0200

    Multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,9 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default  async function handleProfileSignup(firstName, lastName, filename){
-    return Promise.all([ await signUpUser(firstName, lastName), 
-         await uploadPhoto(filename)]).then((values) => {
-            return [{ 
-        
-
-            }]
-
-    })
-
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(filename)]).then((values) => Promise.resolve({
+    [values[0].status]: values[0].value,
+  }));
 }
