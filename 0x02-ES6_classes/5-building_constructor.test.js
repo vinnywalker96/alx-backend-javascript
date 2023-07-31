c59632258373081287a265f6d51ba82a058990de
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-07-31 13:10:08 +0200

    SkyHighBuilding

--- 0x02-ES6_classes/5-building_constructor.test.js
+++
@@ -1,6 +0,0 @@
-import Building from "./5-building.js";
-
-test("Building is implemented correctly", () => {
-  const building = new Building(200);
-  expect(building.sqft).toBe(200);
-});
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,8 +1,8 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
+  constructor(sqrt, floors) {
+    super(sqrt);
     this._floors = floors;
   }
 
