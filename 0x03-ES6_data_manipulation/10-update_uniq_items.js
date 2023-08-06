069cc9a1c0dcf1365e7561974a7d1c1f7a590e5b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-06 14:38:01 +0200

    update unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (map instanceof Map) {
     const updatedMap = map.forEach((value, key) => {
       if (value === 1) {
-        map.set(key, 10);
+        map.set(key, 100);
       }
     });
     return updatedMap;
