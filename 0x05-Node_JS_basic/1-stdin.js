1cf9e1465d45e575d19c90d6452fbfd6e6542ef5
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-22 15:56:01 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,6 +5,9 @@ const readline = require('readline').createInterface({
 
 readline.question('Welcome to Holberton School, what is your name? \n', (name) => {
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
   readline.close();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
