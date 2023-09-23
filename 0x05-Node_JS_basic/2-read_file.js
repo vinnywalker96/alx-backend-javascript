f8a96ff350ee43d345bbf7fb8ea38445464716db
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-23 16:52:30 +0200

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,26 +1,38 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
   const students = {};
-  const field = {}
-  let numberOfStudents = 0;
+  const fields = {};
+  let length = 0;
   try {
-	const data = fs.readFileSync(path, 'utf-8');
-	const lines = data.toString().split('\n');
-	for (let i = 0; i < lines.length; i += 1){
-	  if (lines[1]) {
-		  numberOfStudents += 1;
-		  const field = lines[i].toString().split(',');
-		  if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-			  students[field[3]].push(field[0]);
-		  } else {
-			  students[fields
-
-	}
-	console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-	} catch(err){
-	  throw new Error('Cannot load the database');
-	}
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
 }
-module.exports = countStudents;
 
+module.exports = countStudents;
