745e0ce4852b1b8cdac09a5a99ca480e32f8513c
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-03 15:08:44 +0200

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 function calculateNumber(a, b) {
   const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
+  const roundedB = Math.ceil(b);
 
   return roundedA + roundedB;
 }
