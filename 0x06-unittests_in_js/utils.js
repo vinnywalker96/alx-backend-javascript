9bda4ebeb17e5e82eb13b1ef7789f1daade4b38d
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-05 09:52:20 +0200

    Stub

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -2,7 +2,7 @@ const Utils = {
     calculateNumber: function (type, a, b) {
       switch (type) {
         case 'SUM':
-          return Math.round(a) + Math.round(b) + 40;
+          return Math.round(a) + Math.round(b) ;
         case 'SUBTRACT':
           return Math.round(a) - Math.round(b);
         case 'DIVIDE':
