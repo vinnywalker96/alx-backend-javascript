e60d145a58f4123398709efedd0e076b36102e5c
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-05 09:50:45 +0200

    Stub

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,25 +1,17 @@
-class Utils {
-    static calculateNumber(type, a, b) {
-	const an = Math.round(a);
-	const bn = Math.round(b);
-	let c = 0;
-	switch (type) {
-	case 'SUM':
-            c = an + bn;
-            break;
-	case 'SUBTRACT':
-            c = an - bn;
-            break;
-	case 'DIVIDE':
-	    if (bn === 0) {
-		c = "Error";
-	    } else {
-		c = an / bn;
-	    }
-	    break;
-	}
-	return c;
+const Utils = {
+    calculateNumber: function (type, a, b) {
+      switch (type) {
+        case 'SUM':
+          return Math.round(a) + Math.round(b) + 40;
+        case 'SUBTRACT':
+          return Math.round(a) - Math.round(b);
+        case 'DIVIDE':
+          if (Math.round(b) === 0) {
+            return 'ERROR';
+          }
+          return Math.round(a) / Math.round(b);
+      }
     }
 }
-
+  
 module.exports = Utils;
