5b33a38037fcc63d8da5e940375b2b71fe5225fb
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-06 17:21:21 +0000

    Basic Intergration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,14 +5,14 @@ const request = require('request');
 describe('Index page', () => {
   const endpoint = 'http://localhost:7865';
 
-  it('Returns status of 200 Ok', function (done) {
-    request(endpoint, function (error, response, body) {
+  it('Returns status  code of 200 Ok',  (done) => {
+    request(endpoint,  (error, response, body)  => {
       expect(response.statusCode).to.equal(200);
       done();
     });
 
-    it('Returns the right content', function (done) {
-        request(endpoint, function (error, response, body) {
+    it('Returns Correct page constenr',  (done) => {
+        request(endpoint,  (error, response, body) => {
           expect(body).to.equal('Welcome to the payment system');
           done();
         });
