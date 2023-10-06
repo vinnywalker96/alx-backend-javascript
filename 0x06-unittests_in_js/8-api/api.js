1cfe1f88eb4196750facbdf69bd8982225c7f2f4
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-06 16:50:28 +0000

    Basic Intergration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 7865;
-const hostname = '172.17.0.2';
+const express = require('express')
+const app = express()
+const port = 7865
 
-app.get('/', (res, req) => {
-	res.send('Welcome to the payment system');
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system')
+})
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`)
 });
-
-app.listen(port, hostname,  () => {
-	console.log(`API available on localhost port ${port}`);
-});
-
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,4 +1,19 @@
-const assert = require('assert');
-const app = require('./api.js');
-const chai = require('chai');
+const chai = require("chai");
+const request = require("supertest");
+const app = require('./api');
+const expect = chai.expect;
 
+describe('Index page', () => {
+        it('Should  return  200 OK', async () => {
+            const response = await request(app)
+            .get("/")
+            .expect(200);
+        });
+        it('Should return content of Welcome to the payment system', async () => {
+            const response = await request(app)
+            .get('/');
+
+            const body = response.body;
+            chai.assert(body, 'Welcome to the payment system');
+        })
+});
